7220d374964b3f9ef640f22e0305c19daee97c78
Santiago Bravo <67444457+sbravo15@users.noreply.github.com>
2022-04-06 13:19:20 -0600

    Add files via upload

---
+++ WeeklyCode/Week1/TuesApr5/TueApr5/src/scripts/index.js
@@ -0,0 +1,7 @@
+//Reference Manual MDN -> Search "MDN JavaScript Console" -> Results[Methods, Syntax etc.]
+//Use the documentation and google it
+
+
+console.log("Hello, Wolrd!");
+
+console.error("Este es un Error, ayuda!");
